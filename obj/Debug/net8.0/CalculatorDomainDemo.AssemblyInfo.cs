84334e424e5471cab78f77112b4907ff7f46e977
TJ Gaba <tjgaba@outlook.com>
2026-02-01 23:19:06 +0200

    Initial commit

--- obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5b0376b82e5cd0bb758fcee4ab8c6b9213c3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f89736073da9853feffbcb4dbe4d278ba7b05e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
